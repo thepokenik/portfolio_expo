e69274820a809c24861be226c87b8ff9f7f774f4
thepokenik <nikolaspedro.np@gmail.com>
2023-09-19 18:09:16 -0300

    Feat: Scroll reveal page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+//<!-- ============== Written Animation ================ -->
 new TypeIt("#simpleUsage", {
     speed: 50,
     waitUntilVisible: true,
 }).go();
 
+
+//<!-- ============== Dark/White Theme ================ -->
 const chk = document.getElementById('checkboxInput');
 const socialLinks = document.querySelectorAll('.socialLink');
 
@@ -38,6 +41,40 @@ chk.addEventListener('change', () => {
 });
 
 
+//<!-- ============== Scroll Reveal================ -->
+
+// window.sr = ScrollReveal({ reset: true });
+
+// sr.reveal('.projects-list', {duration: 1000})
+// sr.reveal('.section-header', {duration: 1000})
+// sr.reveal('.project-section', {duration: 1000})
+// sr.reveal('.project', {duration: 1000})
+// sr.reveal('.project-img', {duration: 1000})
+// sr.reveal('.project-img img', {duration: 1000})
+// sr.reveal('.project-content', {duration: 1000})
+// sr.reveal('.project-description', {duration: 1000})
+// sr.reveal('.btn-group button', {duration: 1000})
+// sr.reveal('.btn', {duration: 1000})
+// sr.reveal('.btn i', { duration: 1000 })
 
 
+//<!-- ============== Scroll Bar================ -->
+const checkboxInput = document.getElementById('checkboxInput');
 
+checkboxInput.addEventListener('change', function () {
+  const isDarkMode = checkboxInput.checked;
+
+  // Função para atualizar a cor da barra de rolagem com base no estado do switch
+  function updateScrollbarColor() {
+    const scrollbarThumbColor = isDarkMode ? 'black' : '#6bdb43';
+
+    // Aplicar a cor à barra de rolagem
+    document.documentElement.style.setProperty('--scrollbar-thumb-color', scrollbarThumbColor);
+  }
+
+  // Chame a função para atualizar a cor da barra de rolagem quando o switch for alterado
+  updateScrollbarColor();
+});
+
+// Execute a função para configurar a cor inicial com base no estado inicial do switch
+updateScrollbarColor();
